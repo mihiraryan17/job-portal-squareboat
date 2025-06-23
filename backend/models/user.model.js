3a02ed81975f1d5233404dc8a2287810be1144f8
Mihir <mihiraryan1728@gmail.com>
2025-06-23 20:08:10 +0530

    Initial clean commit

---
+++ backend/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["candidate", "recruiter"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+export const User = mongoose.model("User", userSchema);
---
+++ backend/routes/company.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import {
+  getCompany,
+  getCompanyById,
+  registerCompany,
+} from "../controllers/company.controller.js";
+
+const router = express.Router();
+
+router.route("/register").post(isAuthenticated, registerCompany);
+router.route("/get").get(isAuthenticated, getCompany);
+router.route("/get/:id").get(isAuthenticated, getCompanyById);
+
+export default router;
